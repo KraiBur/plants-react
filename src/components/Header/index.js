8153efd32b608fb0937bf92195b17b4f5cf75df5
KraiBur <152266525+KraiBur@users.noreply.github.com>
2024-02-28 14:07:51 +0500

    plants

---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import { useCart } from '../../hooks/useCart';
+
+import headerStyles from './Header.module.scss';
+
+function Header(props) {
+  const { totalPrice } = useCart();
+
+    return(
+      <header className="d-flex justify-between align-center p-40">
+        <Link to="/plants-react/">
+          <div className="d-flex align-center">
+              <img className={headerStyles.headerLogo} src="img/logo.svg" alt="Logotype" />
+            <div>
+              <h3 className="text-uppercase"></h3>
+              <p className="opacity-5"></p>
+            </div>
+          </div>
+        </Link>
+        <ul className="d-flex">
+          <li onClick={props.onClickCart} className="mr-30 cu-p">
+            <img className={headerStyles.headerCart} src="img/cart.svg" alt="Корзина" />
+            <span>{totalPrice} руб.</span>
+          </li>
+          <li className="mr-20 cu-p">
+            <Link to="/plants-react/favorites">
+              <img className={headerStyles.headerHeart} src="img/heart.svg" alt="Закладки" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/plants-react/orders">
+              <img className={headerStyles.headerUser} src="img/user.svg" alt="Пользователь" />
+            </Link>
+          </li>
+        </ul>
+      </header>
+    );
+}
+
+export default Header;
